116708136f721d043f7bc632f955d8ad1c66040e
Justin Walraven <j.walraven@eagles.oc.edu>
2025-01-18 16:33:59 -0600

    Jobs model

---
+++ app/Models/Job.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+class Job {
+    public static function all(){
+        return [
+            [
+                'id' => '1',
+                'title' => 'Director',
+                'salary' => '$50,000',
+            ],
+            [
+                'id' => '2',
+                'title' => 'Programmer',
+                'salary' => '$10,000',
+            ],
+            [
+                'id' => '3',
+                'title' => 'Teacher',
+                'salary' => '$40,000',
+            ],
+                
+        ];
+    }
+    public static function find(int $id): array{
+
+        $job = \Illuminate\Support\Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+        if(! $job){
+            abort(404);
+        }
+        return $job;
+    }
+
+}
